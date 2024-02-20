227176925acabe6b45ed3dc93ce0de1eb1b0960a
nowead <nowead814@gmail.com>
2024-02-20 09:44:52 +0900

    commit 1

---
+++ src/main/java/edu/fisa/lab/Step06CltestApplication.java
@@ -0,0 +1,13 @@
+package edu.fisa.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step06CltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step06CltestApplication.class, args);
+	}
+
+}
